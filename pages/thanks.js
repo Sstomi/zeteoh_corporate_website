91e879567d47172d494e9bc43cb6e996960f8c6b
Sstomi <40359766+Sstomi@users.noreply.github.com>
2022-06-20 21:52:38 +0900

    feat: add thank page for contactform

---
+++ pages/thanks.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import Navbar from "../components/Navbar";
+import Message from "../components/Message";
+
+
+import Footer from "../components/Footer";
+
+const Thanks = () => {
+  const router = useRouter();
+  const { t } = useTranslation("common");
+
+  return (
+    <div>
+      <main>
+        <Navbar />
+        <Message />
+        
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export const getStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ["common", "hero"])),
+  },
+});
+
+export default Thanks;
