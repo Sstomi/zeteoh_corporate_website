7bf2a3a783c2ee20f046a98b69ede8c7a0a7dcad
Yann <yann.leguilly@gmail.com>
2022-06-21 21:37:56 +0900

    chore: remove gtag

--- pages/_app.js
+++ pages/_app.js
@@ -2,27 +2,29 @@ import { appWithTranslation } from "next-i18next";
 import "tailwindcss/tailwind.css";
 import Script from "next/script";
 
-const MyApp = ({ Component, pageProps }) => {
-  return (
-    <>
-      <div className="container">
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-EP8V6PRVC6"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
-  
-            gtag('config', 'G-EP8V6PRVC6');
-          `}
-        </Script>
-      </div>
-      <Component {...pageProps} />
-    </>
-  );
-};
+// const MyApp = ({ Component, pageProps }) => {
+//   return (
+//     <>
+//       <div className="container">
+//         <Script
+//           src="https://www.googletagmanager.com/gtag/js?id=G-EP8V6PRVC6"
+//           strategy="afterInteractive"
+//         />
+//         <Script id="google-analytics" strategy="afterInteractive">
+//           {`
+//             window.dataLayer = window.dataLayer || [];
+//             function gtag(){window.dataLayer.push(arguments);}
+//             gtag('js', new Date());
+
+//             gtag('config', 'G-EP8V6PRVC6');
+//           `}
+//         </Script>
+//       </div>
+//       <Component {...pageProps} />
+//     </>
+//   );
+// };
+
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default appWithTranslation(MyApp);
