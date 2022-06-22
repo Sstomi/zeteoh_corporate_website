0ce4b3a318af05df3b98e8f2f431e7a21094f9d7
Sstomi <40359766+Sstomi@users.noreply.github.com>
2022-06-22 22:21:50 +0900

    fix seo tag

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
-import { NextSeo } from 'next-seo';
+import { NextSeo } from "next-seo";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-
+import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Merit from "../components/Merit";
@@ -14,39 +14,43 @@ const Homepage = () => {
   return (
     <div>
       <>
-    <NextSeo
-      title="zeteoh, Inc."
-      description="We detect human activities on wearables with AI."
-      canonical="https://www.canonical.ie/"
-      openGraph={{
-        url: 'https://www.zeteoh.com/',
-        title: 'zeteoh株式会社',
-        description: 'AIを使ってウェアラブルで身体活動を検知する',
-        images: [
-          {
-            url: 'https://www.zeteoh.com/Home/card-01.png',
-            width: 800,
-            height: 600,
-            alt: 'Og Image Alt',
-            type: 'image/jpeg',
-          },
-          {
-            url: 'https://www.zeteoh.com/Home/card-02.png',
-            width: 900,
-            height: 800,
-            alt: 'Og Image Alt Second',
-            type: 'image/jpeg',
-          },
-        ],
-        site_name: 'zeteoh',
-      }}
-      twitter={{
-        handle: '@Satomi48650478',
-        site: '@zeteoh_ai',
-        cardType: 'summary_large_image',
-      }}
-    />
-  </>
+        <NextSeo
+          title="zeteoh, Inc."
+          description="We detect human activities on wearables with AI."
+          canonical="https://www.canonical.ie/"
+          openGraph={{
+            url: "https://www.zeteoh.com/",
+            title: "zeteoh株式会社",
+            description: "AIを使ってウェアラブルで身体活動を検知する",
+            images: [
+              {
+                url: "https://www.zeteoh.com/images/Home/card-01.png",
+                width: 800,
+                height: 600,
+                alt: "Og Image Alt",
+                type: "image/jpeg",
+              },
+              {
+                url: "https://www.zeteoh.com/images/Home/card-02.png",
+                width: 900,
+                height: 800,
+                alt: "Og Image Alt Second",
+                type: "image/jpeg",
+              },
+            ],
+            site_name: "zeteoh",
+          }}
+          twitter={{
+            handle: "@Satomi48650478",
+            site: "@zeteoh_ai",
+            cardType: "summary_large_image",
+          }}
+        />
+      </>
+      <Head>
+        <title>zeteoh, Inc.</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <main>
         <Navbar />
         <Hero />
